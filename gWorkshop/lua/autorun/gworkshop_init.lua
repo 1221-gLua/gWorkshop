370b9160006f1dcd05bd367dbf5eeb7afa07d781
1221 <88312923+1221-gLua@users.noreply.github.com>
2022-05-16 16:48:46 +0300

    Update gworkshop_init.lua

--- gWorkshop/lua/autorun/gworkshop_init.lua
+++ gWorkshop/lua/autorun/gworkshop_init.lua
@@ -1,10 +1,10 @@
-gWorkshop = gWorkshop or {}
-gWorkshop.Settings = gWorkshop.Settings or {}
-
 do
     if SERVER then
         AddCSLuaFile('gworkshop/fetching_clientstate.lua')
 
+        gWorkshop = gWorkshop or {}
+        gWorkshop.Settings = gWorkshop.Settings or {}
+        
         include('gworkshop/config.lua')
         include('gworkshop/processing_preload.lua')
         include('gworkshop/processing_postload.lua')
